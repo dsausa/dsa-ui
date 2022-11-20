1619aea7ccf308fed9e006327130886bd238588e
Charles Hughes <charles@binti.com>
2022-11-20 15:19:16 -0800

    Add jest eslint plugin

--- wc/.eslintrc.js
+++ wc/.eslintrc.js
@@ -1,30 +1,28 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:@stencil/recommended"
-    ],
-    "overrides": [
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module",
-        "project": "./tsconfig.json",
-        "tsconfigRootDir": __dirname,
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-        // I am a truthy/falsy enjoyer. Sue me, Stencil.
-        "@stencil/strict-boolean-conditions": "off",
-        // This rule seems to be broken currently.
-        "@stencil/decorators-context": "off",
-        "@typescript-eslint/no-explicit-any": "off",
-    },
-}
+  env: {
+    'browser': true,
+    'es2021': true,
+    'jest/globals': true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@stencil/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['@typescript-eslint', 'jest'],
+  rules: {
+    // I am a truthy/falsy enjoyer. Sue me, Stencil.
+    '@stencil/strict-boolean-conditions': 'off',
+    // This rule seems to be broken currently.
+    '@stencil/decorators-context': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+};
