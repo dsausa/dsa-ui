500924d30366993dc4d7b38cf3f90258bb271828
Yuri Sulyma <yuri.sulyma@protonmail.com>
2022-11-25 21:37:58 -0500

    Allow stories to be located in subdirs

--- wc/.storybook/main.js
+++ wc/.storybook/main.js
@@ -1,12 +1,10 @@
 module.exports = {
-  "stories": [
-    "../stories/*.stories.@(mdx|js|jsx|ts|tsx)"
+  stories: ['../stories/**/*.stories.@(mdx|js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    'storybook-dark-mode',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "storybook-dark-mode"
-  ],
-  "framework": "@storybook/web-components",
-  "staticDirs": [{ from: '../dist/components/assets', to: 'assets/' }]
-}
+  framework: '@storybook/web-components',
+  staticDirs: [{ from: '../dist/components/assets', to: 'assets/' }],
+};
