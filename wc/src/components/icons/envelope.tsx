08913d3efc90ecb5a27ef8bf4b530f9655bc3696
Erik Stockmeier <9088720+erikdstock@users.noreply.github.com>
2022-12-05 09:40:14 -0500

    add color prop (default currentcolor)

--- wc/src/components/icons/envelope.tsx
+++ wc/src/components/icons/envelope.tsx
@@ -1,4 +1,4 @@
-import { Component, h } from '@stencil/core';
+import { Component, h, Prop } from '@stencil/core';
 
 @Component({
   tag: 'dsa-envelope-icon',
@@ -6,9 +6,18 @@ import { Component, h } from '@stencil/core';
   shadow: true,
 })
 export class EnvelopeIcon {
+  /**
+   * Icon color (currentColor by default)
+   */
+  @Prop() color = 'currentColor';
+
   render() {
     return (
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      <svg
+        style={{ fill: this.color }}
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+      >
         <path d="M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z" />
         <path d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z" />
       </svg>
